d70d9aad8522e09000f43308d1e091dacfe3d132
vshivam05 <sv699291@gmail.com>
2025-05-27 23:29:19 +0530

    90

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:8082/tasks"; // Adjust the URL to your backend
+// const API_BASE_URL = "http://localhost:8082/tasks"; // Adjust the URL to your backend
+const API_BASE_URL = "https://task-manager-dpay.onrender.com/tasks"; // Adjust the URL to your backend
 
 export async function fetchTasks() {
   try {
